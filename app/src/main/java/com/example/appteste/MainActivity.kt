a4ba7e1eb27ec47a5292f9695bdc87b1a598c7cc
IsaacYusuke <isaacyusuke@usp.br>
2023-12-07 20:37:00 -0300

    App botão

--- app/src/main/java/com/example/appteste/MainActivity.kt
+++ app/src/main/java/com/example/appteste/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.appteste
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+//import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+//import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -25,9 +31,10 @@ fun MyApp() {
     var counter by remember { mutableStateOf(0) }
 
     // Material Design surface container
-    Surface(color = MaterialTheme.colorScheme.inverseOnSurface) {
+    Surface(modifier = Modifier.fillMaxSize() , color = MaterialTheme.colorScheme.inverseOnSurface) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
+            verticalArrangement = Arrangement.Center)
+        {
             Text(text = "You have pressed the button this many times:")
             Text(text = "$counter", style = MaterialTheme.typography.titleLarge)
             Button(onClick = { counter++ }) {
