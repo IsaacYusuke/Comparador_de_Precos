26994ee6a4e0d4f01d2ce3cc0a422d5e6612d157
IsaacYusuke <isaacyusuke@usp.br>
2023-12-07 20:19:17 -0300

    App botão

--- app/src/main/java/com/example/appteste/MainActivity.kt
+++ app/src/main/java/com/example/appteste/MainActivity.kt
@@ -3,42 +3,45 @@ package com.example.appteste
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.appteste.ui.theme.AppTesteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTesteTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Isaac")
-                    //GreetingPreview()
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun MyApp() {
+    // A variable that holds the state of the counter
+    var counter by remember { mutableStateOf(0) }
+
+    // Material Design surface container
+    Surface(color = MaterialTheme.colorScheme.inverseOnSurface) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(text = "You have pressed the button this many times:")
+            Text(text = "$counter", style = MaterialTheme.typography.titleLarge)
+            Button(onClick = { counter++ }) {
+                Text("Somar 1")
+            }
+            Button(onClick = { counter-- }) {
+                Text("Diminuir 1")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AppTesteTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    MyApp()
 }
