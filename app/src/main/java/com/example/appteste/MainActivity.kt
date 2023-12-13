3c07843b5258fc80845b7f01411d7e89f6426016
IsaacYusuke <isaacyusuke@usp.br>
2023-12-13 19:12:20 -0300

    Teste com surface e column

--- app/src/main/java/com/example/appteste/MainActivity.kt
+++ app/src/main/java/com/example/appteste/MainActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-//import androidx.compose.foundation.background
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-//import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-//import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +34,22 @@ fun MyApp() {
     var counter by remember { mutableStateOf(0) }
 
     // Material Design surface container
-    Surface(modifier = Modifier.fillMaxSize() , color = MaterialTheme.colorScheme.inverseSurface) {
+    Surface(modifier = Modifier.fillMaxHeight() , color = MaterialTheme.colorScheme.inverseSurface) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center)
+        {
+            Text(text = "Vezes que o botão foi pressionado")
+            Text(text = "$counter", style = MaterialTheme.typography.titleLarge)
+            Button(onClick = { counter++ }) {
+                Text("Somar 1")
+            }
+            Button(onClick = { counter-- }) {
+                Text("Diminuir 1")
+            }
+        }
+    }
+
+    Surface(modifier = Modifier.fillMaxWidth() , color = MaterialTheme.colorScheme.error) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center)
         {
