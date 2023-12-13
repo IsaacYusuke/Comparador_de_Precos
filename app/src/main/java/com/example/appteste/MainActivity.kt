00f04fd7a6ff6939484bcad2852b941a64bb7180
IsaacYusuke <isaacyusuke@usp.br>
2023-12-13 19:50:40 -0300

    Teste campo para digitar número

--- app/src/main/java/com/example/appteste/MainActivity.kt
+++ app/src/main/java/com/example/appteste/MainActivity.kt
@@ -12,12 +12,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +59,8 @@ fun MyApp() {
             verticalArrangement = Arrangement.Center)
         {
             Text(text = "Vezes que o botão foi pressionado")
-            Text(text = "$counter", style = MaterialTheme.typography.titleLarge)
+            //Text(text = "$counter", style = MaterialTheme.typography.titleLarge)
+            NumberInputField()
             Button(onClick = { counter++ }) {
                 Text("Somar 1")
             }
@@ -64,6 +70,23 @@ fun MyApp() {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NumberInputField() {
+    var number by remember { mutableStateOf("") }
+
+    TextField(number,
+        { newValue ->
+            // Aqui você pode incluir uma lógica para validar a entrada como numérica
+            number = newValue.filter { it.isDigit() }
+        },
+        label = { Text("Digite um número") }
+        )
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
